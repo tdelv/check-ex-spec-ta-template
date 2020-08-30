9826723d4b366f6d28df695aee4e81e2f842b881
Thomas Del Vecchio <53149055+tdelv@users.noreply.github.com>
2020-08-30 00:07:01 -0400

    Update ta-update-assignment.rkt

--- ta-update-assignment.rkt
+++ ta-update-assignment.rkt
@@ -3,19 +3,21 @@
 (require forge/check-ex-spec/ta)
 (require crypto)
 
+(define student-directory "<insert-name-of-student-github-repo>")
+
 (define-values (assignment-name key-path)
   (command-line
     #:args (assignment-name key-path)
     (values assignment-name key-path)))
 
 (define loc (format "~a/~a" assignment-name assignment-name))
-(make-directory* (format "../checkexspec-student/~a" assignment-name))
+(make-directory* (format "~a/~a" student-directory assignment-name))
 (encrypt-file (read-key key-path)
               (format "~a.rkt" loc)
-              (format "../checkexspec-student/~a" loc))
+              (format "~a/~a" student-directory loc))
 
-(system (string-append "cd ../checkexspec-student;"
-               (format "git add ~a;" loc)
-               (format "git commit -m 'Updating assignment ~a.';" assignment-name)
-                       "git pull;"
-                       "git push;"))
+(system (string-append (format "cd ~a;" student-directory)
+                       (format "git add ~a;" loc)
+                       (format "git commit -m 'Updating assignment ~a.';" assignment-name)
+                               "git pull;"
+                               "git push;"))
